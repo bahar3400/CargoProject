c75d73eb7ac59c91a6f08bc29c47dc83f4b0375f
BAHAR EKİNCİ <3235691083@ogr.klu.edu.tr>
2025-09-10 18:04:22 +0300

    Remove ECommerceProject project

--- ECommerceProject/ECommerceProject.Core/Models/Enums/ProductCategory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ECommerceProject.Core.Models.Enums
-{
-    public enum ProductCategory
-    {
-        Electronic=1,
-        Furniture
-    }
-}
--- ECommerceProject/ECommerceProject.Service/Interfaces/ProductInterfaces/IProductInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using ECommerceProject.Core.Bases;
-namespace ECommerceProject.Service.Interfaces.Products
-{
-    public interface IProductService
-    {
-        bool Delete(int id);
-        int GetNextId(ProductBase productBase);
-    }
-}
